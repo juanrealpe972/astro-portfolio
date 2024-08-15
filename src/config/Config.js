fd68ca74a38b0ab3723b5ff5a86d234f6fdc4b6b
juanrealpe972 <juanrealpe972@gmail.com>
2024-08-15 13:03:17 -0500

    Mejorando los estilos del boton para cambiar el color del tema y agregando la configuración para el componente de contacto.

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,30 +1,5 @@
 import i18n from '../data/i18n'
 
-// DarkMode >-----------------------------------------------------------------------------<
-let themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
-let themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
-
-if ( localStorage.getItem("color-theme") === "dark" || (!("color-theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches) ) {
-    themeToggleLightIcon.classList.remove("hidden");
-    document.documentElement.classList.add("dark");
-} else {
-    themeToggleDarkIcon.classList.remove("hidden");
-    document.documentElement.classList.remove("dark");
-}
-
-document.getElementById("theme-toggle").addEventListener("click", function () {
-    themeToggleDarkIcon.classList.toggle("hidden");
-    themeToggleLightIcon.classList.toggle("hidden");
-
-    if (localStorage.getItem("color-theme") === "light") {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("color-theme", "dark");
-    } else {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("color-theme", "light");
-    }
-});
-
 // Movil Toggle >------------------------------------------------------------------------------<
 const menuToggle = document.getElementById("menu-toggle");
 const mobileMenu = document.getElementById("mobile-menu");
